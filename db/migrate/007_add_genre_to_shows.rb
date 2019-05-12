a7e1a9aa3f21e5a7fdc242e6ebd0e38401feb224
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:23:40 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,4 +1,4 @@
-class CreateShows < ActiveRecord::Migration[4.2]
+class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
     create_table :shows do |t|
       #primary key of :id is created for us!
