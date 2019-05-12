ce033b364b9bceefa39a5a9aa7e7fd884c78e0d4
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:10:42 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,8 @@
-class CreateActors < ActiveRecord::Migration
+class CreateCharacters < ActiveRecord::Migration
   def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
+    create_table :characters do |t|
+      t.string :name
+      t.string :show_id
     end
   end
 end
