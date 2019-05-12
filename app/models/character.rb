1bfb61dcfbe016b1e7a3ff8a33fc7e2bfbc6a7e0
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:55:00 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ class Character < ActiveRecord::Base
   
   belongs_to :actor
   belongs_to :show
-  belongs_to :netowkr
+  belongs_to :network
   
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
