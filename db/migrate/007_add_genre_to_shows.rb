78af0208d3f1c9037878a9b0ca73162518a0cc05
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:24:47 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_genre_to_shows.rb
+++ db/migrate/007_add_genre_to_shows.rb
@@ -1,8 +1,6 @@
 class AddGenreToShow < ActiveRecord::Migration[4.2]
   def change
-    create_table :shows do |t|
-      #primary key of :id is created for us!
-      t.string :name
+    add_column :shows, :genre, :string
     end
   end
 end
