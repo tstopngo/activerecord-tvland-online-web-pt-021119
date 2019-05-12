22b4bab618e130cd4282f233393c4cccffb27359
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:49:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
