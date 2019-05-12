21799aa71b4fef20d3b830d7646aa2cf04f1b997
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:54:55 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,16 +2,10 @@ class Character < ActiveRecord::Base
   
   belongs_to :actor
   belongs_to :show
+  belongs_to :netowkr
   
   def say_that_thing_you_say
     "#{self.name} always says: #{self.catchphrase}"
   end
   
-  def build_show(arg)
-    self.show = arg[:name]
-  end
-  
-   def build_network(arg)
-    self.show = arg[:name]
-  end
 end
