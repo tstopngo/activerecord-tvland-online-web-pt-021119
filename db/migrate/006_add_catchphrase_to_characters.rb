63161cecc2d7ad6f50470815871c9bbcf5aa8bc4
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:26:05 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,4 +1,4 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   def change
     add_column :characters, :string, :catchphrase
   end
