706c4db64c367acd37aa33430db0c1a3037cb01a
Tracy Ngo <tngo1206@gmail.com>
2019-05-12 03:36:01 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_actor_id_to_actors.rb
+++ db/migrate/008_add_actor_id_to_actors.rb
@@ -1,8 +1,5 @@
 class CreateActors < ActiveRecord::Migration[4.2]
-  def change
-    create_table :actors do |t|
-      t.string :first_name
-      t.string :last_name
-    end
+   def change
+    add_column :shows, :network_id, :integer
   end
 end
